619d9b83ad62d93864cdaaf178e41a56d393f5af
Young King <bryantbeltr@gmail.com>
2022-07-03 08:01:26 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI-GImons',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
